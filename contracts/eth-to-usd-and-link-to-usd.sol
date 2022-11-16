8e1c12ad6daee9bf409ec6ec44592f4faf0e716f
LearnSmartContracts <117533990+LearnSmartContracts@users.noreply.github.com>
2022-11-16 14:19:54 -0800

    Update eth-to-usd-and-link-to-usd.sol

--- contracts/eth-to-usd-and-link-to-usd.sol
+++ contracts/eth-to-usd-and-link-to-usd.sol
@@ -6,32 +6,48 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract PriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
 
+contract PriceConverter {
+    AggregatorV3Interface internal eth_usd_price_feed;
+    AggregatorV3Interface internal link_usd_price_feed;
+
     /**
      * Network: Goerli
      * Aggregator: ETH/USD
-     * Address: 0x48731cF7e84dc94C5f84577882c14Be11a5B7456
-     * Aggregator: Link/USD
-     */Address: 0xb4c4a493AB6356497713A78FFA6c60FB53517c63
+     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+     * Aggregator: LINK/USD
+     * Address: 0xD627B1eF3AC23F1d3e576FA6206126F3c1Bd0942
+     */
     constructor() {
-        priceFeed = AggregatorV3Interface(
-            0x48731cF7e84dc94C5f84577882c14Be11a5B7456
-        );
+        eth_usd_price_feed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        link_usd_price_feed = AggregatorV3Interface(0xb4c4a493AB6356497713A78FFA6c60FB53517c63);
     }
+
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int) {
+   function getETHUSD() public view returns (int) {
         (
-            ,
-            /*uint80 roundID*/
-            int price, /*uint startedAt*/
-            ,
-            ,
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = eth_usd_price_feed.latestRoundData();
 
-        ) = /*uint timeStamp*/
-            /*uint80 answeredInRound*/
-            priceFeed.latestRoundData();
+        return price;
+    }
+
+
+    function getLINKUSD() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = link_usd_price_feed.latestRoundData();
+
         return price;
     }
 }
